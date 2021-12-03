fc15928f14048ae7d683c432d8d93b4df2b3da1a
Eduardo Flores <luis.flores.ramirez@accenture.com>
2021-12-03 10:46:29 -0600

    Implementado Clase Service

---
+++ src/main/java/com/edflor/test/springboot/app/services/CuentaServiceImpl.java
@@ -0,0 +1,51 @@
+package com.edflor.test.springboot.app.services;
+
+import com.edflor.test.springboot.app.models.Banco;
+import com.edflor.test.springboot.app.models.Cuenta;
+import com.edflor.test.springboot.app.repositories.BancoRepository;
+import com.edflor.test.springboot.app.repositories.CuentaRepository;
+
+import java.math.BigDecimal;
+
+public class CuentaServiceImpl implements CuentaService{
+    private CuentaRepository cuentaRepository;
+    private BancoRepository bancoRepository;
+
+    public CuentaServiceImpl(CuentaRepository cuentaRepository, BancoRepository bancoRepository) {
+        this.cuentaRepository = cuentaRepository;
+        this.bancoRepository = bancoRepository;
+    }
+
+    @Override
+    public int revisarTotalTransferencia(Long id) {
+        Banco banco = bancoRepository.findById(id);
+        return banco.getTotalTransferencia();
+    }
+
+    @Override
+    public Cuenta findById(Long idBanco) {
+        return cuentaRepository.findById(idBanco);
+    }
+
+    @Override
+    public BigDecimal revisarSaldo(Long cuentaId) {
+        Cuenta cuenta = cuentaRepository.findById(cuentaId);
+        return cuenta.getSaldo();
+    }
+
+    @Override
+    public void transferir(Long numeroCuentaOrigen, Long numeroCuentaDestino, BigDecimal monto) {
+        Banco banco = bancoRepository.findById(1L);
+        int totalTransferencia = banco.getTotalTransferencia();
+        banco.setTotalTransferencia(++totalTransferencia);
+        bancoRepository.update(banco);
+
+        Cuenta cuentaOrigen = cuentaRepository.findById(numeroCuentaOrigen);
+        cuentaOrigen.debito(monto);
+        cuentaRepository.update(cuentaOrigen);
+
+        Cuenta cuentaDestino = cuentaRepository.findById(numeroCuentaDestino);
+        cuentaDestino.credito(monto);
+        cuentaRepository.update(cuentaDestino);
+    }
+}
